1aebc3356aef98ba146c792fe03d05b5ac8392ea
Enaium <wyy-666@hotmail.com>
2022-02-20 11:09:41 +0800

    Update Initialize.kt

--- app/src/main/java/cn/enaium/bb/Initialize.kt
+++ app/src/main/java/cn/enaium/bb/Initialize.kt
@@ -23,48 +23,115 @@
 package cn.enaium.bb
 
 import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.util.Log
+import android.view.ContextThemeWrapper
+import android.widget.Toast
 import de.robv.android.xposed.*
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import org.json.JSONObject
+import java.io.File
+import java.io.FileInputStream
 
 
 /**
  * @author Enaium
  */
 class Initialize : IXposedHookLoadPackage {
+
+    private var init = false
+
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
 
-        XposedBridge.log("PACKAGE:${lpparam.packageName}")
-
-        var init = false
-
-        if (arrayListOf(
-                "com.bilibili.azurlane", "com.miHoYo.ys.bilibili", "com.knight.union.bili"
-            ).contains(lpparam.packageName)
-        ) {
-            init = true
-        }
-
-        if (!init) {
-            if (lpparam.packageName.contains("bilibili")) {
-                init = true
-            }
-        }
-
-        if (!init) {
+        if (init) {
             return
         }
 
+        val cGSCPubCommon: Class<*>?
 
-        val cGSCPubCommon = lpparam.classLoader.loadClass("com.gsc.pub.GSCPubCommon")
+        try {
+            cGSCPubCommon = lpparam.classLoader.loadClass("com.gsc.pub.GSCPubCommon")
+        } catch (_: ClassNotFoundException) {
+            return
+        }
+
+        val uidPath = "/storage/emulated/0/bilibili-uid.txt"
+
+        val uid = if (File(uidPath).exists()) {
+            val fileInputStream = FileInputStream(uidPath)
+            String(fileInputStream.readBytes())
+        } else {
+            null
+        }
+
+        var context: Context? = null
+
+        XposedHelpers.findAndHookMethod(
+            ContextThemeWrapper::class.java,
+            "attachBaseContext",
+            Context::class.java,
+            object : XC_MethodHook() {
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    context = param.args[0] as Context
+                    super.beforeHookedMethod(param)
+                }
+            })
 
         XposedHelpers.findAndHookMethod(cGSCPubCommon,
             "startHeart",
             Activity::class.java,
             object : XC_MethodReplacement() {
                 override fun replaceHookedMethod(param: MethodHookParam?): Any? {
-                    XposedBridge.log("取消心跳上报")
+                    XposedBridge.log("BiligameBypass:取消心跳上报")
+                    if (context != null) {
+                        Toast.makeText(context, "BiligameBypass:取消心跳上报", Toast.LENGTH_LONG).show()
+                    }
                     return null
                 }
             })
+
+        XposedHelpers.findAndHookMethod(
+            Application::class.java,
+            "attach",
+            Context::class.java,
+            object : XC_MethodHook() {
+                @Throws(Throwable::class)
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    val classLoader: ClassLoader = (param.args[0] as Context).classLoader
+                    val aResponseBody = classLoader.loadClass("okhttp3.ResponseBody")
+                    XposedHelpers.findAndHookMethod(aResponseBody, "string", object : XC_MethodHook() {
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            if (param.result != null) {
+                                val toString = param.result.toString()
+                                if (toString.startsWith("{") && toString.endsWith("}")) {
+                                    val jsonObject = JSONObject(toString)
+
+                                    //判断为登录
+                                    if (jsonObject.has("code") && jsonObject.has("auth_name")) {
+                                        XposedBridge.log("BiligameBypass:登录")
+
+                                        val user = JSONObject()
+                                        user.put("auth_name", "NULL")
+                                        user.put("realname_verified", 1)
+                                        user.put("remind_status", 0)
+                                        user.put("code", 0)
+                                        user.put("access_key", "NULL")
+                                        user.put("expires", "NULL")
+                                        user.put("uid", uid ?: "NULL")
+                                        user.put("face", "NULL")
+                                        user.put("s_face", "NULL")
+                                        user.put("uname", "NULL")
+                                        user.put("server_message", "NULL")
+                                        param.result = user.toString()
+                                    }
+                                }
+                            }
+                            super.afterHookedMethod(param)
+                        }
+                    })
+                }
+            })
+        init = true
     }
 }
